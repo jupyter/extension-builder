2b6924762df97f86d3aa4797536c488b5c5d630b
Steven Silvester <steven.silvester@ieee.org>
2016-09-08 11:37:04 -0500

    Convert the loader to a typescript class

--- src/loader.ts
+++ src/loader.ts
@@ -1,227 +1,279 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-var semver = require('semver');
-
-/**
- * The registered module cache.
- */
-var registered = {};
-
-/**
- * The installed module cache.
- */
-var installedModules = {};
-
-/**
- * The module name lookup cache.
- */
-var lookupCache = {};
-
-/**
- * A cache of objects for parsed version-mangled module names.
- */
-var nameCache = {};
-
-/**
- * Object to store loaded and loading bundles.
- * "0" means "already loaded".
- * Array means "loading", array contains callbacks.
- */
-var installedBundles = {};
+import {
+  maxSatisfying
+} from 'semver';
 
 
 /**
- * Define a module that can be synchronously required.
- *
- * @param name - The version-mangled fully qualified name of the module.
- *   For example, "foo@1.0.1/lib/bar/baz.js".
- *
- * @param callback - The callback function for invoking the module.
+ * A module loader using semver for deduplication of modules.
  */
-function defineModule(name, callback) {
-  if (!(name in registered)) {
-    registered[name] = callback;
-  }
-}
-
-
-/**
- * Synchronously require a module that has already been loaded.
- *
- * @param name - The semver-mangled fully qualified name of the module.
- *   For example, "foo@^1.1.0/lib/bar/baz.js".
- *
- * @returns The exports of the requested module, if registered.  The module
- *   selected is the registered module that maximally satisfies the semver
- *   range of the request.
- */
-function requireModule(name) {
-  // Check if module is in cache.
-  var moduleId = findModuleId(name);
-  if(installedModules[moduleId]) {
-    return installedModules[moduleId].exports;
+export
+class ModuleLoader {
+  /**
+   * Construct a new module loader.
+   */
+  constructor() {
+    // Add the ensure function to the require module for internal use within
+    // the modules.
+    (this.require as any).e = this.ensureBundle.bind(this);
   }
 
-  // Create a new module (and put it into the cache).
-  var module = installedModules[moduleId] = {
-    exports: {},
-    id: moduleId,
-    loaded: false
-  };
-
-  // Execute the module function.
-  registered[moduleId].call(module.exports, module, module.exports, requireModule);
-
-  // Flag the module as loaded.
-  module.loaded = true;
-
-  // Return the exports of the module.
-  return module.exports;
-}
-
-
-/**
- * Ensure a bundle is loaded on a page.
- *
- * @param path - The public path of the bundle (e.g. "lab/jupyter.bundle.js").
- *
- * @param callback - The callback invoked when the bundle has loaded.
- */
-function ensureBundle(path, callback) {
-  // "0" is the signal for "already loaded"
-  if (installedBundles[path] === 0) {
-    return callback.call(null, requireModule);
+  /**
+   * Define a module that can be synchronously required.
+   *
+   * @param path - The version-mangled fully qualified path of the module.
+   *   For example, "foo@1.0.1/lib/bar/baz.js".
+   *
+   * @param callback - The callback function for invoking the module.
+   */
+  define(path: string, callback: () => void): void {
+    if (!(path in this._registered)) {
+      this._registered[path] = callback;
+    }
   }
 
-  // An array means "currently loading".
-  if (Array.isArray(installedBundles[path])) {
-    installedBundles[path].push(callback);
-    return;
+  /**
+   * Synchronously require a module that has already been loaded.
+   *
+   * @param path - The semver-mangled fully qualified path of the module.
+   *   For example, "foo@^1.1.0/lib/bar/baz.js".
+   *
+   * @returns The exports of the requested module, if registered.  The module
+   *   selected is the registered module that maximally satisfies the semver
+   *   range of the request.
+   */
+  require(path: string): any {
+    // Check if module is in cache.
+    let id = this._findModuleId(path);
+    let installed = this._modules;
+    if (installed[id]) {
+      return installed[id].exports;
+    }
+
+    // Create a new module (and put it into the cache).
+    let mod = installed[id] = {
+      exports: {},
+      id,
+      loaded: false
+    };
+
+    // Execute the module function.
+    let callback = this._registered[id];
+    callback.call(mod.exports, mod, mod.exports, this.require.bind(this));
+
+    // Flag the module as loaded.
+    mod.loaded = true;
+
+    // Return the exports of the module.
+    return mod.exports;
   }
 
-  // Start bundle loading.
-  installedBundles[path] = [callback];
-  var head = document.getElementsByTagName('head')[0];
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.charset = 'utf-8';
-  script.async = true;
-  script.onload = function() {
-    var callbacks = installedBundles[path];
-    while(callbacks.length)
-      callbacks.shift().call(null, requireModule);
-    installedBundles[path] = 0;
+  /**
+   * Ensure a bundle is loaded on a page.
+   *
+   * @param path - The public path of the bundle (e.g. "lab/jupyter.bundle.js").
+   *
+   * @param callback - The callback invoked when the bundle has loaded.
+   */
+  ensureBundle(path: string, callback?: any): Promise<void> {
+    let bundles = this._bundles;
+    let bundle = bundles[path] || this._createBundle(path);
+
+    if (bundle.loaded) {
+      if (callback) {
+        callback.call(null, this.require);
+      }
+      return Promise.resolve(void 0);
+    }
+
+    if (callback) {
+      bundle.callbacks.push(callback);
+    }
+    return bundle.promise;
   }
-  head.appendChild(script);
-  script.src = path;
-}
 
+  /**
+   * Find a module matching a given module request.
+   *
+   * @param path - The semver-mangled fully qualified path to the module.
+   *   For example, "foo@^1.1.0/lib/bar/baz.js".
+   *
+   * @returns The matching defined module path, if registered.  A match is
+   *   the registered path that maximally satisfies the semver range of the
+   *   request.
+   */
+  private _findModuleId(path: string): string {
+    // Use the cached id if available.
+    let cache = this._lookupCache;
+    if (cache[path]) {
+      return cache[path];
+    }
+    let modules = Object.keys(this._registered);
+    let source = this._parsePath(path);
+    if (!source) {
+      throw Error('Invalid module path ' + path);
+    }
+    let matches: string[] = [];
+    let versions: string[] = [];
+    for (let mod of modules) {
+      let target = this._parsePath(mod);
+      if (!target) {
+        continue;
+      }
+      if (source.package === target.package && source.module === target.module) {
+        matches.push(mod);
+        versions.push(target.version);
+      }
+    }
 
-/**
- * Ensure a list of bundles are loaded on the page.
- *
- * @param paths - The public paths of the bundles. (e.g. "lab/foo.bundle.js").
- *
- * @returns A promise that resolves with the require function when all the
- *   bundles have loaded.
- */
-function requireBundles(paths) {
-  var promises = paths.map(function(path) {
-    return new Promise(function (resolve, reject) {
-      ensureBundle(path, resolve);
+    if (!matches.length) {
+      throw Error('No module found matching: ' + path);
+    }
+    let index = 0;
+    if (matches.length > 1) {
+      let best = maxSatisfying(versions, source.version);
+      if (!best) {
+        throw new Error('No module found satisying: ' + path);
+      }
+      index = versions.indexOf(best);
+    }
+    cache[path] = matches[index];
+    return matches[index];
+  }
+
+  /**
+   * Create a bundle record for a path.
+   */
+  private _createBundle(path: string): Private.IBundle {
+    // Start bundle loading.
+    let head = document.getElementsByTagName('head')[0];
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.charset = 'utf-8';
+    script.async = true;
+    let promise = new Promise<void>((resolve, reject) => {
+      script.onload = () => {
+        let record = this._bundles[path];
+        while (record.callbacks.length) {
+          record.callbacks.shift().call(null, this.require);
+        }
+        record.loaded = true;
+        resolve(void 0);
+      };
+      script.onerror = err => {
+        reject(err);
+      };
     });
-  });
-  return Promise.all(promises).then(function () {
-    return requireModule;
-  });
+    head.appendChild(script);
+    script.src = path;
+    let bundle: Private.IBundle = this._bundles[path] = {
+      loaded: false,
+      callbacks: [],
+      promise
+    };
+    return bundle;
+  }
+
+  /**
+   * Parse a version-mangled module path.
+   *
+   * @param path - The module path (e.g. "foo@^1.1.0/lib/bar/baz.js").
+   *
+   * @returns A parsed object describing the module path.
+   */
+  private _parsePath(path: string): Private.IPathInfo {
+    let cache = this._parsed;
+    if (cache[path]) {
+      return cache[path];
+    }
+    let match = path.match(/(^(?:@[^/]+\/)??[^/@]+?)@([^/]+?)(\/.*)?$/);
+    if (!match) {
+      cache[path] = null;
+    } else {
+      cache[path] = {
+        package: match[1],
+        version: match[2],
+        module: match[3]
+      };
+    }
+    return cache[path];
+  }
+
+  private _registered: { [key: string]: () => void } = Object.create(null);
+  private _parsed: { [key: string]: Private.IPathInfo } = Object.create(null);
+  private _modules: { [key: string]: Private.IModule } = Object.create(null);
+  private _bundles: { [key: string]: Private.IBundle } = Object.create(null);
+  private _lookupCache: { [key: string]: string } = Object.create(null);
 }
 
 
 /**
- * Parse a version-mangled module name.
- *
- * @param name - The module name (e.g. "foo@^1.1.0/lib/bar/baz.js").
- *
- * @returns A parsed object describing the module name.
+ * A namespace for private module data.
  */
-function parseName(name) {
-  if (nameCache[name]) {
-    return nameCache[name];
+namespace Private {
+  /**
+   * A module record.
+   */
+  export
+  interface IModule {
+    /**
+     * The exports of the module.
+     */
+    exports: any;
+
+    /**
+     * The id of the module.
+     */
+    id: string;
+
+    /**
+     * Whether the module has been loaded.
+     */
+    loaded: boolean;
   }
-  var match = name.match(/(^(?:@[^/]+\/)??[^/@]+?)@([^/]+?)(\/.*)?$/);
-  if (!match) {
-    nameCache[name] = null;
-  } else {
-    nameCache[name] = {
-      package: match[1],
-      version: match[2],
-      module: match[3]
-    }
+
+  /**
+   * A bundle record.
+   */
+  export
+  interface IBundle {
+    /**
+     * Whether the bundle has been loaded.
+     */
+    loaded: boolean;
+
+    /**
+     * The callbacks associated with the bundle.
+     */
+    callbacks: (() => void)[];
+
+    /**
+     * A promise fullfiled when the bundle has loaded.
+     */
+    promise: Promise<void>;
   }
-  return nameCache[name];
+
+  /**
+   * A parsed path record.
+   */
+  export
+  interface IPathInfo {
+    /**
+     * The source package.
+     */
+    package: string;
+
+    /**
+     * The version string.
+     */
+    version: string;
+
+    /**
+     * The module path.
+     */
+    module: string;
+  }
 }
 
-
-/**
- * Find a module matching a given module request.
- *
- * @param name - The semver-mangled fully qualified name of the module.
- *   For example, "foo@^1.1.0/lib/bar/baz.js".
- *
- * @returns The matching defined module name, if registered.  A match is
- *   the registered name that maximally satisfies the semver range of the
- *   request.
- */
-function findModuleId(name) {
-  // Use the cached id if available.
-  if (lookupCache[name]) {
-    return lookupCache[name];
-  }
-  var modules = Object.keys(registered);
-  // Get the package name, semver string, and module name.
-  var source = parseName(name);
-  if (!source) {
-    throw Error('Invalid module name ' + name);
-  }
-  var matches = [];
-  var versions = [];
-  for (var i = 0; i < modules.length; i++) {
-    var mod = modules[i];
-    var target = parseName(mod);
-    if (!target) {
-      continue;
-    }
-    if (source.package === target.package && source.module === target.module) {
-      matches.push(mod);
-      versions.push(target.version);
-    }
-  };
-
-  if (!matches.length) {
-    throw Error('No module found matching: ' + name);
-    return;
-  }
-  var index = 0;
-  if (matches.length > 1) {
-    var best = semver.maxSatisfying(versions, source.version);
-    if (!best) {
-      throw new Error('No module found satisying: ' + name);
-    }
-    index = versions.indexOf(best);
-  }
-  lookupCache[name] = matches[index];
-  return matches[index];
-}
-
-
-// Add the ensure function to the require module for internal use within
-// the modules.
-requireModule.e = ensureBundle;
-
-
-module.exports = {
-  define: defineModule,
-  require: requireModule,
-  requireBundles: requireBundles
-}
