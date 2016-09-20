16ab94e351234d2e75ef2dc1667a516c0034db98
Steven Silvester <steven.silvester@ieee.org>
2016-09-20 16:45:12 -0400

    Add test framework

---
+++ test/src/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  buildExtension
+} from '../../lib';
+
+
+describe('buildExtension()', () => {
+
+  it('should always pass', () => {
+
+  });
+
+});
---
+++ test/src/typings.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../typings/expect.js/expect.js.d.ts"/>
+/// <reference path="../../typings/mocha/mocha.d.ts"/>
+/// <reference path="../../typings/webpack/webpack.d.ts"/>
