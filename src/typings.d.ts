e9d0133485db9b522f51bd7893250f78833b5408
Jason Grout <jgrout6@bloomberg.net>
2016-10-28 07:16:39 -0400

    Clean up typings.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,3 @@
-/// <reference path="../typings/es6-promise/es6-promise.d.ts"/>
 /// <reference path="../typings/extract-text-webpack-plugin/extract-text-webpack-plugin.d.ts"/>
-/// <reference path="../typings/node/node.d.ts"/>
-/// <reference path="../typings/semver/semver.d.ts"/>
 /// <reference path="../typings/webpack/webpack.d.ts"/>
 /// <reference path="../typings/webpack-config/webpack-config.d.ts"/>
