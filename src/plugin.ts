765de135476d89703a50f509871cb8c24f9246fa
Steven Silvester <steven.silvester@ieee.org>
2017-03-09 18:41:25 -0600

    Remove punyurl hack

--- src/plugin.ts
+++ src/plugin.ts
@@ -452,11 +452,7 @@ namespace Private {
       // Allow patch version increments of local packages.
       semver = `~${sourcePackage.version}`;
     }
-    // Punycode is improperly handled by Webpack - url requires 1.3,
-    // but webpack fulfills it with 1.4.
-    if (name === 'punycode') {
-      semver = '*';
-    }
+
     let id = `${name}@${semver}`;
     if (modPath) {
       modPath = modPath.split(path.sep).join('/');
